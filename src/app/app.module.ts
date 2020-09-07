b45f48c8726bb81d920f985d0102ddce71832302
Alexander Lodygin <lodigin.alexander@gmail.com>
2020-09-07 10:19:31 +0300

    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {environment} from 'src/environments/environment'
 import {TopBarModule} from './shared/modules/topBar/topBar.module'
 import {PersistenceService} from './shared/services/persistence.service'
 import {AuthInterceptor} from './shared/services/auth.interceptor'
+import {GlobalFeedModule} from './globalFeed/globalFeed.module'
 
 const AuthInterceptorProvider = {
   provide: HTTP_INTERCEPTORS,
@@ -27,6 +28,7 @@ const AuthInterceptorProvider = {
     HttpClientModule,
     AuthModule,
     TopBarModule,
+    GlobalFeedModule,
     EffectsModule.forRoot([]),
     StoreModule.forRoot({}),
     StoreDevtoolsModule.instrument({
